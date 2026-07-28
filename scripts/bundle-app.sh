9c972649ab1cc168884794dbdf56b64586bbac31
Giorgiofox <giorgiofox@gmail.com>
2026-07-28 13:29:37 +0200

    Bump to 0.1.2 (packaged app now launches)

--- scripts/bundle-app.sh
+++ scripts/bundle-app.sh
@@ -43,8 +43,8 @@ cat > "$CONTENTS/Info.plist" <<PLIST
     <key>CFBundleIdentifier</key>      <string>local.portsight.app</string>
     <key>CFBundleExecutable</key>      <string>$EXECUTABLE</string>
     <key>CFBundlePackageType</key>     <string>APPL</string>
-    <key>CFBundleShortVersionString</key> <string>0.1.1</string>
-    <key>CFBundleVersion</key>         <string>2</string>
+    <key>CFBundleShortVersionString</key> <string>0.1.2</string>
+    <key>CFBundleVersion</key>         <string>3</string>
     <key>LSMinimumSystemVersion</key>  <string>14.0</string>
 $ICON_LINE
     <!-- Menu-bar agent: no Dock icon, no app-switcher entry. -->
